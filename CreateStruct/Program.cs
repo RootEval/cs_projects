62d0819c17870e5124c0d1accd175b7e2a99f24f
RootEval <44101973+RootEval@users.noreply.github.com>
2020-09-08 17:51:02 +0300

    ex 2

--- CreateStruct/Program.cs
+++ CreateStruct/Program.cs
@@ -2,22 +2,27 @@
 
 namespace CreateStruct
 {
+    
     struct Person
     {
+        public enum Genders : int { Female, Male };
         public string firstName;
         public string lastName;
         public int age;
+        public Genders gender;
 
-        public Person(string _firstName, string _lastName, int _age)
+        public Person(string _firstName, string _lastName, int _age, Genders _gender)
         {
             firstName = _firstName;
             lastName = _lastName; 
             age = _age;
+            gender = _gender;
         }
         public override string ToString()
         {
             //Console.WriteLine("toString\n");
-            return firstName + " " + lastName + ", age " + age;
+            // v1: return firstName + " " + lastName + ", age " + age;
+            return firstName + " " + lastName + " (" + gender + "), age " + age;
         }
 
 
@@ -27,7 +32,7 @@ namespace CreateStruct
     {
         static void Main(string[] args)
         {
-            Person p = new Person("Andrew", "Oleg", 35);
+            Person p = new Person("Andrew", "Oleg", 35, Person.Genders.Male);
             Console.WriteLine(p); // ToString() is called by default
         }
     }
