dd71a631454e64ca3c8d4fa16c61922f4e5991a7
RootEval <44101973+RootEval@users.noreply.github.com>
2020-09-08 18:57:11 +0300

    upload

---
+++ Practive 4/CheckTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CheckTypes {
+    class Program {
+        static void Main(string[] args) {
+            Int16 i16 = 1; // 16-bits
+            Int32 i32 = 1; // 32-bits
+            double db = 1; // 64-bits
+
+            //i16 = i32; // error
+            //i16 = db; // error
+            i32 = i16;
+            //i32 = db; // error
+            db = i16; 
+            db = i32;
+
+            // ошибки возникают при попытке записать переменную типа, 
+            // требующего для хранения больше памяти, чем доступно
+            // типу, в который эта переменная должна быть записана
+        }
+    }
+}
