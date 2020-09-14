df602ced5124abe6a52a36165dd5988233ef76d1
RootEval <44101973+RootEval@users.noreply.github.com>
2020-09-14 23:03:30 +0300

    upload

---
+++ Practice 2/Pracm 2/FileDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace FileDemo {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            StreamWriter writer = File.CreateText(@"C:\newfile.txt"); 
+            writer.WriteLine("This is my new file");
+            writer.WriteLine("Do you like its format?");
+            writer.Close();
+        }
+    }
+}
