ad67c4ec34ca0d5bd626c41c41e637e1a27a8ec0
RootEval <44101973+RootEval@users.noreply.github.com>
2020-09-14 22:59:16 +0300

    upload

---
+++ Practice 2/Pracm 1/FileWatchingDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace FileWatchingDemo {
+    class Program {
+        static void Main(string[] args) {
+            FileSystemWatcher watcher = new FileSystemWatcher(Environment.SystemDirectory);
+            //FileSystemWatcher watcher = new FileSystemWatcher(@"\"); // to test without admin rights
+            watcher.Filter = "*.ini";
+            watcher.IncludeSubdirectories = true;
+            watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.Size;
+            watcher.Changed += new FileSystemEventHandler(watcher_Changed);
+            watcher.EnableRaisingEvents = true;
+            Console.ReadKey(); // force main thread to stay
+        }
+
+        static void watcher_Changed(object sender, FileSystemEventArgs e) {
+            Console.WriteLine("Changed: {0}", e.FullPath);
+        }
+    }
+}
