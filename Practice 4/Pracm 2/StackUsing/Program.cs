ffe04af3c055b1bdd9e0a7c64b9cc2f6fa647a7b
Oleg <Oleg@OLEG-PC>
2020-09-22 14:43:01 +0300

    upload

---
+++ Practice 4/Pracm 2/StackUsing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace StackUsing {
+    class Program {
+        static void Main(string[] args) {
+            Stack stack = new Stack();
+            stack.Push("First");
+            stack.Push("Second");
+            stack.Push("Third");
+            stack.Push("Fourth");
+            while (stack.Count > 0) {
+                object obj = stack.Pop();
+                Console.WriteLine("From Stack: {0}", obj);
+            }
+
+        }
+    }
+}
