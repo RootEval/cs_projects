c13c8a2c9e90afaf9abcc12a65eec9ec20e5bcc7
Oleg <Oleg@OLEG-PC>
2020-09-22 14:11:21 +0300

    reupload

--- Practice 3/Pracm 1/RegexProj/Program.cs
+++ Practice 3/Pracm 1/RegexProj/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Text.RegularExpressions;
 
-namespace Lesson1_Exercise1_CS {
+namespace RegexProject {
     class Program {
         static void Main(string[] args) {
             string[] input = { "(555)555-1212",
@@ -13,9 +11,13 @@ namespace Lesson1_Exercise1_CS {
                               "01111",
                               "01111-1111",};
             foreach (string s in input) {
-                if (IsPhone(s)) Console.WriteLine(s + " is a phone number");
-                else if (IsZip(s)) Console.WriteLine(s + " is a zip code");
-                else Console.WriteLine(s + " is unknown");
+                if (IsPhone(s)) Console.WriteLine(ReformatPhone(s) + " is a phone number");
+                else
+                    if (IsZip(s))
+                    Console.WriteLine(s + " is a zip code");
+                else
+                    Console.WriteLine(s + " is unknown");
+
             }
             Console.ReadLine();
         }
@@ -23,6 +25,10 @@ namespace Lesson1_Exercise1_CS {
         static bool IsPhone(string s) {
             return Regex.IsMatch(s, @"^\(?\d{3}\)?[\s\-]?\d{3}\-?\d{4}$");
         }
+        static string ReformatPhone(string s) {
+            Match m = Regex.Match(s, @"^\(?(\d{3})\)?[\s\-]?(\d{3})\-?(\d{4})$"); return String.Format("({0}) {1}-{2}", m.Groups[1], m.Groups[2], m.Groups[3]);
+        }
+
 
         static bool IsZip(string s) {
             return Regex.IsMatch(s, @"^\d{5}(\-\d{4})?$");
--- Practice 3/Pracm 2/ConsoleApp1/Properties/AssemblyInfo.cs
+++ Practice 3/Pracm 2/ConsoleApp1/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4b5383e1-a450-4682-9ab6-08f7e5bb871d")]
+[assembly: Guid("bd8288c3-5d7d-40ed-b981-73a5a7d89d25")]
 
 // Version information for an assembly consists of the following four values:
 //
