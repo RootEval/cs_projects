1d663dae795e888c2bdc58fbaa052182b249fc52
Oleg <Oleg@OLEG-PC>
2020-09-22 14:33:01 +0300

    upload

--- Practice 3/Pracm 2/ChangeFileEncoding/Program.cs
+++ Practice 3/Pracm 2/ChangeFileEncoding/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1 {
+namespace ChangeFileEncoding {
     class Program {
         static void Main(string[] args) {
             StreamReader sr = new StreamReader(@"boot.ini");
--- Practice 3/Pracm 2/ChangeFileEncoding/Properties/AssemblyInfo.cs
+++ Practice 3/Pracm 2/ChangeFileEncoding/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("ChangeFileEncoding")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("ChangeFileEncoding")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bd8288c3-5d7d-40ed-b981-73a5a7d89d25")]
+[assembly: Guid("dbd6ab18-21a1-41be-be89-248e332c74cc")]
 
 // Version information for an assembly consists of the following four values:
 //
